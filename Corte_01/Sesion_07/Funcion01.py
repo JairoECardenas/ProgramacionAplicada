cc1437af5cbc852a17de4c355577c59f35359a92
JairoECardenas <jecardenasf@academia.usbbog.edu.co>
2023-08-23 11:30:04 -0500

    Sesion 7 de clase

---
+++ Corte_01/Sesion_07/Funcion01.py
@@ -0,0 +1,17 @@
+# …
+# .===</PYTHON>===.
+# .===</JAIROCKF>===.
+# …
+# This code was created by Jairo Cárdenas, a mechatronic engineering student.
+
+#FUncion con parametros y retorno
+def suma (a,b):
+    resultado = a+b
+    return resultado
+
+print("Inicio del programa.")
+a = int(input("Ingrese un número: "))
+b = int(input("Ingrese otro número: "))
+resultado = suma(a,b)
+print(resultado)
+print("Fin del programa.")
