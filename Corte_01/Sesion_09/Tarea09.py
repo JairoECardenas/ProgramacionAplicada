4f9fe6346f2ed9334ec4d7af051368c3f0832353
JairoECardenas <jecardenasf@academia.usbbog.edu.co>
2023-09-06 15:04:36 -0500

    Primer punto de la tarea 09 finalizado

--- Corte_01/Sesion_09/Tarea09.py
+++ Corte_01/Sesion_09/Tarea09.py
@@ -6,14 +6,23 @@
 from MatrizRandom import crearMatriz, imprimir
 
 def MinMax(matriz):
-    menor = []
-    mayor = []
+    menor = 101
+    mayor = -1
+    posmin=[]
+    posMax=[]
     for i in matriz:
-        menor.append(min(i))
-        mayor.append(max(i))
-    menor = min(menor)
-    mayor = max(mayor)
-    print(f"\nEl menor número de la lista es {menor} y el mayor número es {mayor}")
+        for j in range(len(i)):
+            if min(i) == i[j] and menor >= i[j]:
+                menor = i[j]
+            elif max(i) == i[j] and mayor <= i[j]:
+                mayor = i[j]
+    for i in matriz:
+        for j in range(len(i)):
+            if menor==i[j]:
+                posmin.append([matriz.index(i),i.index(i[j])])  
+            elif mayor==i[j]:
+                posMax.append([matriz.index(i),i.index(i[j])])
+    print(f"\nEl menor número de la lista es {menor} en la posición {posmin} y el mayor número es {mayor} en la posición {posMax}.")
 
 def main():
     matriz = crearMatriz(5,10)
