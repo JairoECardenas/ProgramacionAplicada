c197ed8524a4a1eb752ed3b948ca19b986dd9d63
JairoECardenas <jecardenasf@academia.usbbog.edu.co>
2023-09-14 20:55:31 -0500

    Tarea 10 finalizada (Por ahora)

--- Corte_02/Sesion_10/Tarea10.py
+++ Corte_02/Sesion_10/Tarea10.py
@@ -3,6 +3,10 @@
 # .===</JAIROCKF>===.
 # …
 # This code was created by Jairo Cárdenas, a mechatronic engineering student.
+
+def imprimir_menu():
+    print("\n1) Mostrar la lista de productos disponibles.",\
+        "\n2) Calcular el valor base de un producto.\n3) salir.")
 def imprimir():
     f=open("Alimentos.txt","rt")
     listaAlimentos=f.read()
@@ -20,20 +24,31 @@ def importarDatos():
         datos[b[0]]=[b[1],b[2]]
     return datos
 
-def buscar_valor_base(datos, nombreAlimento, valorNeto):
-    for llave,valor in datos.items():
+def buscar_valor_base(datos, nombreAlimento):
+    for valor in datos.values():
         if valor[0]==nombreAlimento:
+            valorNeto=int(input('Ingrese el valor neto del producto: '))
             valorBase=(valorNeto)/(1+float(valor[1]))
             return round(valorBase,2)
-    return None
+    print("\nHa ingresado un valor inválido.")
+    return False
 
 def main():
-    imprimir()
     datos=importarDatos()
-    alimento=input('Ingrese el nombre del alimento tal y como aparece en la lista: ')
-    valor=int(input('Ingrese el valor neto del producto: '))
-    valorBase=buscar_valor_base(datos, alimento, valor)
-    print(f"El valor base es: ${valorBase}")
+    while True:
+        imprimir_menu()
+        opc=input("\nIngrese la opción que desea ejecutar: ")
+        if opc=='1':
+            imprimir()
+        elif opc=='2':    
+            alimento=input('Ingrese el nombre del alimento tal y como aparece en la lista: ')
+            valorBase=buscar_valor_base(datos, alimento)
+            if valorBase!=False:
+                print(f"El valor base es: ${valorBase}")
+        elif opc=='3' or opc.lower()=='salir':
+            break
+        else:
+            print("Error, selección inválida.")
 
 if __name__=="__main__":
     main()
