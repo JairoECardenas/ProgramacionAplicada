eee5654823bc833a5e2c7a864a168ace73e868ec
JairoECardenas <jecardenasf@academia.usbbog.edu.co>
2023-10-24 18:38:57 -0500

    Finalización Tarea 15

--- Corte_03/Sesion_15/Tarea15.py
+++ Corte_03/Sesion_15/Tarea15.py
@@ -56,8 +56,8 @@ class Mecatronico(Ciudadano):
         self.campo1 = campo1
         self.campo2 = campo2
 
-    def metodo_unico(self):
-        pass
+    def metodo_unico_mecatronico(self):
+        print(f'{self.getNombre()} es un gran Ingeniero a pesar de acumular {self.campo2} horas en lol.')
 
     def mostrar(self):
         info_base = super().mostrar()
@@ -70,8 +70,8 @@ class Piloto(Ciudadano):
         self.campo1 = campo1
         self.campo2 = campo2
 
-    def metodo_unico(self):
-        pass
+    def metodo_unico_piloto(self):
+        print(f'{self.getNombre()} ha realizado {self.campo1()} a sus {self.getEdad()} años de edad.')
 
     def mostrar(self):
         info_base = super().mostrar()
@@ -83,8 +83,8 @@ class Artista(Ciudadano):
         self.campo1 = campo1
         self.campo2 = campo2
 
-    def metodo_unico(self):
-        pass
+    def metodo_unico_artista(self):
+        print(f'Los cuadros que realiza {self.getNombre()} en {self.campo1()} son espectaculares.')
 
     def mostrar(self):
         info_base = super().mostrar()
@@ -93,7 +93,7 @@ class Artista(Ciudadano):
 def mostrar_mayores_menores(lista):
     for p in lista:
         print('\n------------------------')
-        print(p.mostrar())
+        print(p.getNombre())
         if p.es_mayor_de_edad():
             print('Es mayor de edad.')
         else:
@@ -122,14 +122,38 @@ def agregar(lista):
     print('Ciudadano registrado con éxito.')
     print('------------------------')
 
+def seleccionar_profesion_y_metodo(lista):
+    print('Profesiones disponibles:')
+    profesiones = ['Mecatronico', 'Piloto', 'Artista']
+    for i, profesion in enumerate(profesiones):
+        print(f'{i+1}: {profesion}')
+    indice_profesion = int(input('Seleccione una profesión: '))-1
+    
+    print('Lista de ciudadanos registrados:')
+    instancias = [p for p in lista if type(p).__name__ == profesiones[indice_profesion]]
+    for i, instancia in enumerate(instancias):
+        print(f'{i+1}: {instancia.getNombre()}')
+    indice_instancia = int(input('Seleccione un ciudadano: '))-1
+    
+    if profesiones[indice_profesion] == 'Mecatronico':
+        instancias[indice_instancia].metodo_unico_mecatronico()
+    elif profesiones[indice_profesion] == 'Piloto':
+        instancias[indice_instancia].metodo_unico_piloto()
+    elif profesiones[indice_profesion] == 'Artista':
+        instancias[indice_instancia].metodo_unico_artista()
+    else:
+        print('Ha seleccionado un valor inválido')
+
 def main():
     persona=[]
     opc=''
     menu = {'1': lambda: agregar(persona), '2': lambda: Ciudadano.mostrar_todos(persona),
-            '3': lambda: mostrar_mayores_menores(persona), '4': None}
+            '3': lambda: mostrar_mayores_menores(persona),
+            '4': lambda: seleccionar_profesion_y_metodo(persona), '5': None}
     while True:
         print('\n','Clase Estudiantes'.center(26,'-'))
-        print('1) Agregar un núevo ciudadano.\n2) Mostrar los datos guardados.\n3) Conocer si es mayor o menor.\n4) Salir.')
+        print('1) Agregar un núevo ciudadano.\n2) Mostrar los datos guardados.\n3) Conocer si es mayor o menor.' \
+              '\n4) Ejecutar un metodo único.\n5) Salir.')
         opc=input('Dígite la opción que desea ejecutar: ')
         if opc in menu:
             if menu[opc] is None:
